384f5354d004284ebff5d0e9bbd918ea92dcefd5
Brian Figueroa <scissors520@gmail.com>
2022-03-19 21:45:25 -0700

    Add tie condition that I missed

--- js.js
+++ js.js
@@ -26,7 +26,10 @@ function determineWinner() {
         alert(`The computer won the match with ${computerWins} wins to your ${playerWins} with ${ties} ties(s)!`)
     } else if (ties === 5) {
         alert("Surprisingly, the match ended with 5 ties! No winner.")
-    } else {
+    } else if (computerWins === playerWins) {
+        alert(`You tied the match with ${playerWins} and ${computerWins} computer wins with ${ties}!`)
+    } 
+    else {
         alert(`You won the match with ${playerWins} wins to the computers ${computerWins} with ${ties} tie(s)!`)
     }
     alert("Please reload the page to play again.")

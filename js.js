b6d59c48377f37d511787eef7f48ece739bf25ec
Brian Figueroa <scissors520@gmail.com>
2022-03-19 20:35:05 -0700

    Add win/lose logic

--- js.js
+++ js.js
@@ -1,8 +1,26 @@
+//Index 0 = choice and index 1 = loses to
 let choices = [["rock", "paper"], ["paper", "scissors"], ["scissors", "rock"]];
 
 function computerPlay() {
-    return choices[Math.floor(Math.random() * (3 - 1 + 1)) + 1]
+    return choices[Math.floor(Math.random() * (2 - 1 + 1)) + 1]
 }
 
-const computerSelection = computerPlay().toUpperCase;
-const playerSelection = prompt("Rock paper or scissors?: ")
+const computerSelection = computerPlay()
+const playerSelection = prompt("Rock, paper or scissors?: ").toLowerCase()
+
+function playRound(computerSelection, playerSelection) {
+    if(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== 'scissors') {
+        console.log("Please choose rock, paper or scissors; reload the page to play again.")
+        return
+    }
+
+    if(computerSelection[0] === playerSelection) {
+        return `Computer chose ${computerSelection[0]}. Tie!`
+    } else if(computerSelection[1] === playerSelection) {
+        return `Computer chose ${computerSelection[0]}. You win!`
+    } else {
+        return `Computer chose ${computerSelection[0]}. You lose!`
+    }
+}
+
+alert(playRound(computerSelection, playerSelection))

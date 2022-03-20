15f59350cc0bfdf40de09371819fe4a879c5312d
Brian Figueroa <scissors520@gmail.com>
2022-03-19 21:36:04 -0700

    Finish rock paper scissors

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # odin-rock-paper-scissors-js
-The Odin Projects first JavaScript challenge - rock, paper, scissors
+The Odin Projects first JavaScript challenge - rock paper scissors
+
+I will not be making a front end for this.
+
+I realized _too late_ that I was missing commits when I should have been doing them. I will need to work on this in the future. 
--- js.js
+++ js.js
@@ -1,26 +1,60 @@
-//Index 0 = choice and index 1 = loses to
-let choices = [["rock", "paper"], ["paper", "scissors"], ["scissors", "rock"]];
+//Index 0 = choice and index 1 = choice loses to
+let choices = [["rock", "paper"], ["paper", "scissors"], ["scissors", "rock"]]
 
+//Computer choses its move
 function computerPlay() {
     return choices[Math.floor(Math.random() * (2 - 1 + 1)) + 1]
 }
 
-const computerSelection = computerPlay()
-const playerSelection = prompt("Rock, paper or scissors?: ").toLowerCase()
-
+//Plays a single round and keeps score
 function playRound(computerSelection, playerSelection) {
-    if(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== 'scissors') {
-        console.log("Please choose rock, paper or scissors; reload the page to play again.")
-        return
-    }
-
-    if(computerSelection[0] === playerSelection) {
-        return `Computer chose ${computerSelection[0]}. Tie!`
-    } else if(computerSelection[1] === playerSelection) {
-        return `Computer chose ${computerSelection[0]}. You win!`
+    if (computerSelection[0] === playerSelection) {
+        alert(`Computer chose ${computerSelection[0]} you tie this round!`)
+        ties++
+    } else if (computerSelection[1] === playerSelection) {
+        alert(`Computer chose ${computerSelection[0]} you win this round!`)
+        playerWins++
     } else {
-        return `Computer chose ${computerSelection[0]}. You lose!`
+        alert(`Computer chose ${computerSelection[0]} you lose this round!`)
+        computerWins++
     }
 }
 
-alert(playRound(computerSelection, playerSelection))
+//Prompts the player the outcome of the match
+function determineWinner() {
+    if (computerWins > playerWins) {
+        alert(`The computer won the match with ${computerWins} wins to your ${playerWins} with ${ties} ties(s)!`)
+    } else if (ties === 5) {
+        alert("Surprisingly, the match ended with 5 ties! No winner.")
+    } else {
+        alert(`You won the match with ${playerWins} wins to the computers ${computerWins} with ${ties} tie(s)!`)
+    }
+    alert("Please reload the page to play again.")
+}
+
+//Scoreboard
+let computerWins = 0
+let playerWins = 0
+let ties = 0
+
+//Plays up to 5 rounds by calling playRound()
+for (let i = 1; i <= 5; i++) {
+    let computerSelection = computerPlay()
+    let playerSelection = prompt("Rock, paper or scissors?: ").toLowerCase()
+
+    //Check for valid move; if invalid the player forfeits the match because I'm too lazy to add logic to re-do the round
+    if(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== 'scissors') {
+        alert("Please choose rock, paper or scissors; You are disqualified from the match. Please reload the page to play again.")
+        break
+    }
+
+    playRound(computerSelection, playerSelection)
+
+    // Determine winner if all 5 rounds have been played; or if 3 wins from either side have been determined because it would be impossible for the losing player to catch up at this point
+    if(i === 5 || computerWins === 3 || playerWins === 3) {
+        determineWinner()
+        break
+    }
+}
+
+
